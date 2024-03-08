9edaf74d95ed2b9864856769cc5040851f25e5c0
Ruben <adolfolopezmaster@gmail.com>
2024-03-07 21:02:21 -0600

    tengo que ver el pedo que no me cambia color azul

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('America/Lima');
 session_start();
-define('RUTA', 'http://localhost/asistencia/');
+define('RUTA', 'http://localhost/asistenciagerardo/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_NAME', 'Asistencia');
--- views/includes/header.php
+++ views/includes/header.php
@@ -21,7 +21,7 @@
   <link rel="stylesheet" href="<?php echo RUTA . 'assets/'; ?>css/jquery-ui.min.css">
   
   <!-- endinject -->
-  <link rel="shortcut icon" href="<?php echo RUTA . 'assets/'; ?>images/logo.png" />
+  <link rel="shortcut icon" href="<?php echo RUTA . 'assets/'; ?>images/logo.svg" />
 </head>
 <body>
   <div class="container-scroller d-flex">
@@ -91,8 +91,8 @@
             <span class="mdi mdi-menu"></span>
           </button>
           <div class="navbar-brand-wrapper">
-            <a class="navbar-brand brand-logo" href="plantilla.php"><img src="<?php echo RUTA . 'assets/'; ?>images/logo.png" width="60" alt="logo"/></a>
-            <a class="navbar-brand brand-logo-mini" href="plantilla.php"><img src="<?php echo RUTA . 'assets/'; ?>images/logo.png" width="30" alt="logo"/></a>
+            <a class="navbar-brand brand-logo" href="plantilla.php"><img src="<?php echo RUTA . 'assets/'; ?>images/logo.svg" width="240" alt="logo"/></a>
+            <a class="navbar-brand brand-logo-mini" href="plantilla.php"><img src="<?php echo RUTA . 'assets/'; ?>images/logo.svg" width="120" alt="logo"/></a>
           </div>
           <h4 class="font-weight-bold mb-0 d-none d-md-block mt-1">Bienvenido al Sistema de control de Asistencia</h4>
           <ul class="navbar-nav navbar-nav-right">
